65514bb9b575b84e39aad1f726e7bb74c2e06c6b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-08-31 20:48:53 +0200

    Added error controller to test exception behaviour

---
+++ SimpleWebApiService/SimpleWebApiService/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SimpleWebApiService.Controllers
+{
+    public class ErrorController  : ApiController
+    {
+        /// <summary>
+        /// Call this action in order to force an exception being thrown.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            throw new ApplicationException("This is an intended exception in order to verify that all exception logging works as intended.");
+        }
+    }
+}
