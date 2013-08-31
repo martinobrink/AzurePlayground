4358a985a175f97de43b8f6d29cb47889b1eebce
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-08-31 20:52:02 +0200

    another noop commit in order to help nuget package restore

--- SimpleWebApiService/SimpleWebApiService/Controllers/ErrorController.cs
+++ SimpleWebApiService/SimpleWebApiService/Controllers/ErrorController.cs
@@ -9,7 +9,7 @@ namespace SimpleWebApiService.Controllers
         /// <summary>
         /// Call this action in order to force an exception being thrown.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Throws an ApplicationException</returns>
         public IEnumerable<string> Get()
         {
             throw new ApplicationException("This is an intended exception in order to verify that all exception logging works as intended.");
